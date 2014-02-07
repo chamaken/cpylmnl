bef531145cb194509190900fe74ca27c0bc681d7
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2014-02-07 11:48:13 +0900

    cleanup: select exporting

--- cpylmnl/__init__.py
+++ cpylmnl/__init__.py
@@ -4,9 +4,11 @@ from __future__ import absolute_import
 
 from .libmnlh import *
 
-from .attr import *
-from .nlmsg import *
-from .socket import *
+# from .attr import *
+from .attr import attr_parse_payload
+# from .nlmsg import *
+from .nlmsg import nlmsg_put_header
+# from .socket import *
 from .callback import *
 
 from .py_class import *
