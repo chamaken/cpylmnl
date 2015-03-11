5ac39853f8c8a8042c3f0addeeb0c82ea4df390e
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2015-03-12 08:22:52 +0900

    examples: fix to work in mmaped TX
    and update to use new API

--- examples/mmap/rtnl/rtnl-link-dump.py
+++ examples/mmap/rtnl/rtnl-link-dump.py
@@ -15,19 +15,19 @@ import cpylmnl as mnl
 log = logging.getLogger(__name__)
 
 
-@mnl.mnl_attr_cb_t
+@mnl.attribute_cb
 def data_attr_cb(attr, tb):
-    attr_type = mnl.attr_get_type(attr)
+    attr_type = attr.get_type()
 
     # skip unsupported attribute in user-space
     try:
-        mnl.attr_type_valid(attr, if_link.IFLA_MAX)
+        attr.type_valid(if_link.IFLA_MAX)
     except OSError as e:
         return mnl.MNL_CB_OK
 
-    ftbl = {if_link.IFLA_ADDRESS: lambda x: mnl.attr_validate(x, mnl.MNL_TYPE_BINARY),
-            if_link.IFLA_MTU:     lambda x: mnl.attr_validate(x, mnl.MNL_TYPE_U32),
-            if_link.IFLA_IFNAME:  lambda x: mnl.attr_validate(x, mnl.MNL_TYPE_STRING)}
+    ftbl = {if_link.IFLA_ADDRESS: lambda x: x.validate(mnl.MNL_TYPE_BINARY),
+            if_link.IFLA_MTU:     lambda x: x.validate(mnl.MNL_TYPE_U32),
+            if_link.IFLA_IFNAME:  lambda x: x.validate(mnl.MNL_TYPE_STRING)}
     try:
         ftbl.get(attr_type, lambda a: (0, None))(attr)
     except OSError as e:
@@ -38,9 +38,9 @@ def data_attr_cb(attr, tb):
     return mnl.MNL_CB_OK
 
 
-@mnl.mnl_cb_t
+@mnl.header_cb
 def data_cb(nlh, tb):
-    ifm = mnl.nlmsg_get_payload_as(nlh, rtnl.Ifinfomsg)
+    ifm = nlh.get_payload_as(rtnl.Ifinfomsg)
     print("index=%d type=%d flags=%d family=%d " % (ifm.index, ifm.type, ifm.flags, ifm.family), end='')
 
     if ifm.flags & ifh.IFF_RUNNING:
@@ -49,13 +49,13 @@ def data_cb(nlh, tb):
         print("[NOT RUNNING] ", end='')
 
     tb = dict()
-    mnl.attr_parse(nlh, ifm.csize(), data_attr_cb, tb)
+    nlh.parse(ifm.csize(), data_attr_cb, tb)
     if if_link.IFLA_MTU in tb:
-        print("mtu=%d " % mnl.attr_get_u32(tb[if_link.IFLA_MTU]), end='')
+        print("mtu=%d " % (tb[if_link.IFLA_MTU].get_u32()), end='')
     if if_link.IFLA_IFNAME in tb:
-        print("name=%s " % mnl.attr_get_str(tb[if_link.IFLA_IFNAME]), end='')
+        print("name=%s " % (tb[if_link.IFLA_IFNAME].get_str()), end='')
     if if_link.IFLA_ADDRESS in tb:
-        hwaddr = mnl.attr_get_payload_v(tb[if_link.IFLA_ADDRESS])
+        hwaddr = (tb[if_link.IFLA_ADDRESS].get_payload_v())
         print("hwaddr=%s" % ":".join("%02x" % i for i in hwaddr), end='')
 
     print()
@@ -91,7 +91,7 @@ def main():
         nl.map_ring()
         txring = nl.get_ring(mnl.MNL_RING_TX)
         frame = txring.get_frame()
-        buf = mnl.MNL_FRAME_PAYLOAD(frame)
+        buf = mnl.MNL_FRAME_PAYLOAD(frame, frame_size)
 
         nlh = mnl.nlmsg_put_header(buf, mnl.Header)
         nlh.type = rtnl.RTM_GETLINK
